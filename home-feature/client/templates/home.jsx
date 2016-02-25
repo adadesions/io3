e524d118b0c9aef15ab3aba2ffdcd71075b38c53
Augisticz <augisticz@gmail.com>
2016-02-25 22:38:46 +0700

    Edit intro page

--- home-feature/client/templates/home.jsx
+++ home-feature/client/templates/home.jsx
@@ -1,41 +1,11 @@
 Home = React.createClass({
   // mixins: [ReactMeteorData],
 
-  componentDidMount(){
-    $('.ui.sidebar').sidebar({
-        transition: 'overlay'
-    });
 
-   },
-
-  showNav(){
-     $('.ui.sidebar')
-       .sidebar('toggle')
-   },
 
   render(){
     return(
-      <div claccName="home-feature">
-        <div className="ui right demo vertical inverted sidebar labeled icon menu">
-          <a className="item">
-            SIGN UP
-          </a>
-          <a className="item" href="#introSegment">
-            HOME
-          </a>
-          <a className="item" href="#whatAdaSegment">
-            ADACODE.IO
-          </a>
-          <a className="item" href="#courseSegment">
-            COURSES
-          </a>
-          <a className="item" href="#aboutUsSegment">
-            ABOUT US
-          </a>
-          <a className="item" href="#contactSegment">
-            CONTACT US
-          </a>
-        </div>
+      <div className="home-feature">
         <div className="pusher">
           <Intro />
           <WhatAda />
--- home-feature/client/templates/intro.jsx
+++ home-feature/client/templates/intro.jsx
@@ -3,8 +3,23 @@ Intro = React.createClass({
   componentDidMount(){
     videojs('vdo-bg').ready(function() {
       this.play();
+    }),
+
+    $('.ui.sidebar').sidebar({
+        transition: 'overlay'
+    }),
+
+    $('.ui.sticky').sticky({
+      context: '#introSegment'
     })
+
   },
+
+  showNav(){
+     $('.ui.sidebar')
+       .sidebar('toggle')
+   },
+
 
   render(){
     return(
@@ -13,15 +28,38 @@ Intro = React.createClass({
              <source src="/videos/intro/intro-test-1.webm" type="video/webm"/>
              <track kind="captions" src="captions.vtt" srcLang="en" label="English" />
           </video>
-        <div className="logo">
-          <a href="#introSegment"><img src="/images/adacode-home-logo.png"/></a>
+          <div id="navBar" className="nav-bar">
+            <div className="logo">
+              <a href="#introSegment"><img src="/images/intro/adacode-home-logo-blue.png"/></a>
+            </div>
+            <img src="/images/nav-arrow.png" className="nav-button" onMouseOver={this.showNav}/>
+          </div>
+          <div className="content-intro">
+            <h1>ADACODE.IO</h1>
+            <h3>SCHOOL . BUILD . INNOVATION</h3>
+          </div>
+
+          <div className="ui right demo vertical inverted sidebar labeled icon menu">
+            <a className="item">
+              SIGN UP
+            </a>
+            <a className="item" href="#introSegment">
+              HOME
+            </a>
+            <a className="item" href="#whatAdaSegment">
+              ADACODE.IO
+            </a>
+            <a className="item" href="#courseSegment">
+              COURSES
+            </a>
+            <a className="item" href="#aboutUsSegment">
+              ABOUT US
+            </a>
+            <a className="item" href="#contactSegment">
+              CONTACT US
+            </a>
+          </div>
         </div>
-        <img src="/images/nav-arrow.png" className="nav-button" onMouseOver={this.showNav}/>
-        <div className="content-intro">
-          <h1>ADACODE.IO</h1>
-          <h3>SCHOOL . BUILD . INNOVATION</h3>
-        </div>
-      </div>
     )
   }
 })
