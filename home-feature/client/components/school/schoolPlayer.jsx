0e6ef9ee3b5b806fa3698be3b085e097205ae563
Augisticz <augisticz@gmail.com>
2016-03-10 17:59:46 +0700

    Change card color in school

---
+++ home-feature/client/components/school/schoolPlayer.jsx
@@ -0,0 +1,24 @@
+Player = React.createClass({
+
+  componentDidMount(){
+    $( document ).ready(function() {
+      Materialize.fadeInImage('.school-player')
+    })
+  },
+
+  render(){
+    return(
+      <div className="school-player-segment">
+        <NavigatorSchool />
+        <div className="school-player">
+          <iframe width="853" height="480" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
+        </div>   
+        <div className="school-player-name">
+          <h2>
+            Javascript Level 1 Part 1
+          </h2>
+        </div>
+      </div>
+    )
+  }
+})
