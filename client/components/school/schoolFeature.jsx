1199f67043a4f33c65ed6609b38a3907d28429f9
augisticz <augisticz@gmail.com>
2016-03-12 19:02:53 +0700

    Redesign school

--- client/components/school/schoolFeature.jsx
+++ client/components/school/schoolFeature.jsx
@@ -1,28 +1,42 @@
 School = React.createClass({
 
   componentDidMount(){
-    $('#courseOne').hover(function(){
-      $('#courseOne').toggleClass('z-depth-4')
+
+    $(document).ready(function(){
+      $('.parallax').parallax()
     }),
 
-    $('#courseTwo').hover(function(){
-      $('#courseTwo').toggleClass('z-depth-4')
-    }),
+    (function($) {
+    "use strict"
 
-    $('#courseThree').hover(function(){
-      $('#courseThree').toggleClass('z-depth-4')
-    }),
+    var $navbar = $(".nav"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height()
 
-    $('#courseFour').hover(function(){
-      $('#courseFour').toggleClass('z-depth-4')
-    }),
+    //scroll top 0 sticky
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > 0) {
+          $navbar.addClass("sticky");
+        } else {
+          $navbar.removeClass("sticky");
+        }
+    })
 
-    $('#courseFive').hover(function(){
-      $('#courseFive').toggleClass('z-depth-4')
-    }),
+    //section sticky
+    /*$(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        if ($(window).height() > scrollTop) {
+          $navbar.removeClass("sticky");
+        } else {
+          $navbar.addClass("sticky");
+        }
+    });*/
 
-    $('#courseSix').hover(function(){
-      $('#courseSix').toggleClass('z-depth-4')
+    })(jQuery, undefined)
+
+    $(".menu").click(function(){
+      $("#nav").toggleClass("open");
     })
   },
 
@@ -30,74 +44,211 @@ School = React.createClass({
     return(
       <div className="school-blog">
         <NavigatorSchool />
-        <div className="container">
-          <div className="row">
-            <div className="col s12 m8">
-              <div className="section">
-                <a href="/school/path">
-                  <div id="courseOne" className="card-panel grey darken-1 js-item">
-                    <span className="white-text">
-                      <h1>JS</h1>
-                      <h4>Javascript</h4>
-                    </span>
-                  </div>
-                </a>
-              </div>
-              <div className="section">
-                <div className="col s12 m4">
-                  <a href="#">
-                    <div id="courseTwo" className="card-panel grey darken-1">
-                      <span className="white-text">
-                        <h1>JS</h1>
-                        <h4>Javascript</h4>
-                      </span>
-                    </div>
-                  </a>
+        <div className="header-school">
+          <div className="section">
+            <div className="container">
+              <nav id="nav" className="nav">
+                <button className="menu">
+                  <em className="hamburger"></em>
+                </button>
+                <div className="brand">
+                  <a href="#"><img src="images/intro/adacode-home-logo.png"/></a>
                 </div>
-                <div className="col s12 m4">
-                  <a href="#">
-                    <div id="courseThree" className="card-panel grey darken-1">
-                      <span className="white-text">
-                        <h1>JS</h1>
-                        <h4>Javascript</h4>
-                      </span>
-                    </div>
-                  </a>
-                </div>
-                <div className="col s12 m4">
-                  <a href="#">
-                    <div id="courseFour" className="card-panel grey darken-1">
-                      <span className="white-text">
-                        <h1>JS</h1>
-                        <h4>Javascript</h4>
-                      </span>
-                    </div>
-                  </a>
-                </div>
-              </div>
+                <ul className="navbar">
+                  <li>
+                    <a href="#home">Home</a>
+                  </li>
+                  <li>
+                    <a href="#about">About</a>
+                  </li>
+                  <li>
+                    <a href="#services">Services</a>
+                  </li>
+                  <li>
+                    <a href="#contact">Contact</a>
+                  </li>
+                </ul>
+              </nav>
             </div>
-            <div className="col s12 m4">
-              <div className="section">
-                <a href="#">
-                  <div id="courseFive" className="card-panel grey darken-1">
-                    <span className="white-text">
-                      <h1>EP</h1>
-                      <h4>English for Programer</h4>
-                    </span>
-                  </div>
-                </a>
-                <a href="#">
-                  <div id="courseSix" className="card-panel grey darken-1 semantic-item">
-                    <span className="white-text">
-                      <h1>S</h1>
-                      <h4>Semantic User Interface</h4>
-                    </span>
-                  </div>
-                </a>
+          </div>
+          <div className="section">
+            <div className="school-content">
+              <div className="container">
+                <div className="school-content-blog">
+                  <h1>Welcome to AdaCode.io Skool</h1>
+                  <h1>Keys of success are too many, but one key of failure is "UNEDUCATED"</h1>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="parallax-container">
+          <div className="parallax"><img className="background-newcourse" src="/images/school/homepage-school/course-bg-sub1.png"/></div>
+            <div className="section recommended-course">
+              <h1>Recommended Courses</h1>
+              <div className="row">
+                <div className="container">
+                  <div className="container">
+                    <div className="col s12 m4">
+                      <div className="card">
+                        <div className="card-image waves-effect waves-block waves-light">
+                          <img className="activator" src="images/school/course-item/meteor.png"/>
+                        </div>
+                        <div className="card-content">
+                          <span className="card-title activator grey-text text-darken-4">Meteor<i className="material-icons right">more_vert</i></span>
+                          <p><a href="/school/path">This is a link</a></p>
+                        </div>
+                        <div className="card-reveal">
+                          <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col s12 m4">
+                      <div className="card">
+                        <div className="card-image waves-effect waves-block waves-light">
+                          <img className="activator" src="images/school/course-item/s.png"/>
+                        </div>
+                        <div className="card-content">
+                          <span className="card-title activator grey-text text-darken-4">Semantic<i className="material-icons right">more_vert</i></span>
+                          <p><a href="#">This is a link</a></p>
+                        </div>
+                        <div className="card-reveal">
+                          <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col s12 m4">
+                      <div className="card">
+                        <div className="card-image waves-effect waves-block waves-light">
+                          <img className="activator" src="images/school/course-item/react-js.png"/>
+                        </div>
+                        <div className="card-content">
+                          <span className="card-title activator grey-text text-darken-4">React<i className="material-icons right">more_vert</i></span>
+                          <p><a href="#">This is a link</a></p>
+                        </div>
+                        <div className="card-reveal">
+                          <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="all-course">
+            <h1>All Courses</h1>
+            <div className="row">
+              <div className="container">
+                <div className="container">
+                  <div className="col s12 m4">
+                    <div className="card">
+                      <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src="images/school/course-item/js.png"/>
+                      </div>
+                      <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">JavaScript<i className="material-icons right">more_vert</i></span>
+                        <p><a href="/school/path">This is a link</a></p>
+                      </div>
+                      <div className="card-reveal">
+                        <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col s12 m4">
+                    <div className="card">
+                      <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src="images/school/course-item/ep.png"/>
+                      </div>
+                      <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">English<i className="material-icons right">more_vert</i></span>
+                        <p><a href="#">This is a link</a></p>
+                      </div>
+                      <div className="card-reveal">
+                        <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col s12 m4">
+                    <div className="card">
+                      <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src="images/school/course-item/web.png"/>
+                      </div>
+                      <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">Web<i className="material-icons right">more_vert</i></span>
+                        <p><a href="#">This is a link</a></p>
+                      </div>
+                      <div className="card-reveal">
+                        <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="container">
+                <div className="container">
+                  <div className="col s12 m4">
+                    <div className="card">
+                      <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src="images/school/course-item/meteor.png"/>
+                      </div>
+                      <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">Meteor<i className="material-icons right">more_vert</i></span>
+                        <p><a href="#">This is a link</a></p>
+                      </div>
+                      <div className="card-reveal">
+                        <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col s12 m4">
+                    <div className="card">
+                      <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src="images/school/course-item/s.png"/>
+                      </div>
+                      <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">Semantic<i className="material-icons right">more_vert</i></span>
+                        <p><a href="#">This is a link</a></p>
+                      </div>
+                      <div className="card-reveal">
+                        <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col s12 m4">
+                    <div className="card">
+                      <div className="card-image waves-effect waves-block waves-light">
+                        <img className="activator" src="images/school/course-item/react-js.png"/>
+                      </div>
+                      <div className="card-content">
+                        <span className="card-title activator grey-text text-darken-4">React<i className="material-icons right">more_vert</i></span>
+                        <p><a href="#">This is a link</a></p>
+                      </div>
+                      <div className="card-reveal">
+                        <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="section footer-school">
+            <Footer />
+          </div>
       </div>
     )
   }
--- client/components/school/schoolPath.jsx
+++ client/components/school/schoolPath.jsx
@@ -3,18 +3,23 @@ Path = React.createClass({
   componentDidMount(){
     $('#levelOne').click(function(){
       Materialize.showStaggeredList('#levelBlog')
+      $('.level-blog').removeClass('hidden')
+    }),
+
+    $(document).ready(function(){
+      $('.parallax').parallax();
     })
+
   },
 
   render(){
     return(
       <div className="path-segment">
         <NavigatorSchool />
-        <div className="container">
           <div className="section">
             <div className="head-course">
-              <img src="/images/about/ceo.png" className="circle" />
-              <div className="card-panel grey darken-1">
+              <img src="/images/school/course-item/js-icon.png" className="circle" />
+              <div className="card-panel pink accent-1">
                 <span className="white-text">
                   <h4>SPEND SOME TIME WITH THIS POWERFUL
                   <br/>SCRIPTING LANGUAGE AND LEARN TO BUILD
@@ -24,10 +29,12 @@ Path = React.createClass({
               </div>
             </div>
           </div>
+
+
           <div className="section">
             <div className="path-blog">
               <div className="col s12 m4">
-                <div id="levelOne" className="waves-effect waves-blue card-panel grey darken-1">
+                <div id="levelOne" className="waves-effect waves-blue card-panel">
                   <span className="white-text">
                     <h1>LEVEL</h1>
                     <h1>1</h1>
@@ -35,7 +42,7 @@ Path = React.createClass({
                 </div>
               </div>
               <div className="col s12 m4">
-                <div id="levelTwo" className="waves-effect waves-blue card-panel grey darken-1">
+                  <div id="levelTwo" className="waves-effect waves-blue card-panel">
                   <span className="white-text">
                     <h1>LEVEL</h1>
                     <h1>2</h1>
@@ -43,7 +50,7 @@ Path = React.createClass({
                 </div>
               </div>
               <div className="col s12 m4">
-                <div id="levelThree" className="waves-effect waves-blue card-panel grey darken-1">
+                <div id="levelThree" className="waves-effect waves-blue card-panel">
                   <span className="white-text">
                     <h1>LEVEL</h1>
                     <h1>3</h1>
@@ -52,41 +59,44 @@ Path = React.createClass({
               </div>
             </div>
           </div>
-          <div className="section">
-            <ul id="levelBlog">
-              <li>
-                <a href="/school/path/player">
-                  <div className="card-panel grey darken-1">
-                    <span className="white-text">
-                      <h1>JAVASCRIPT LEVEL 1 PART 1</h1>
-                      <h2>Learn to manipulate values on the console</h2>
-                    </span>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="/school/path/player">
-                  <div className="card-panel grey darken-1">
-                    <span className="white-text">
-                      <h1>JAVASCRIPT LEVEL 1 PART 2</h1>
-                      <h2>Learn to manipulate values on the console</h2>
-                    </span>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="/school/path/player">
-                  <div className="card-panel grey darken-1">
-                    <span className="white-text">
-                      <h1>JAVASCRIPT LEVEL 1 PART 3</h1>
-                      <h2>Learn to manipulate values on the console</h2>
-                    </span>
-                  </div>
-                </a>
-              </li>
-            </ul>
+
+
+          <div className="section level-blog hidden">
+            <div className="container">
+              <ul id="levelBlog">
+                <li>
+                  <a href="/school/path/player">
+                    <div id="part1" className="card-panel">
+                      <span className="white-text">
+                        <h1>JAVASCRIPT LEVEL 1 PART 1</h1>
+                        <h2>Learn to manipulate values on the console</h2>
+                      </span>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a href="/school/path/player">
+                    <div id="part2" className="card-panel">
+                      <span className="white-text">
+                        <h1>JAVASCRIPT LEVEL 1 PART 2</h1>
+                        <h2>Learn to manipulate values on the console</h2>
+                      </span>
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a href="/school/path/player">
+                    <div id="part3" className="card-panel">
+                      <span className="white-text">
+                        <h1>JAVASCRIPT LEVEL 1 PART 3</h1>
+                        <h2>Learn to manipulate values on the console</h2>
+                      </span>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
       </div>
     )
   }
