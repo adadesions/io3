487c997ce8428ddf51476d3fd3fd006f569841eb
augisticz <augisticz@gmail.com>
2016-03-12 19:46:06 +0700

    Redesign navbar school

--- client/components/school/schoolFeature.jsx
+++ client/components/school/schoolFeature.jsx
@@ -56,9 +56,6 @@ School = React.createClass({
                 </div>
                 <ul className="navbar">
                   <li>
-                    <a href="#home">Home</a>
-                  </li>
-                  <li>
                     <a href="#about">About</a>
                   </li>
                   <li>
@@ -76,7 +73,7 @@ School = React.createClass({
               <div className="container">
                 <div className="school-content-blog">
                   <h1>Welcome to AdaCode.io Skool</h1>
-                  <h1>Keys of success are too many, but one key of failure is "UNEDUCATED"</h1>
+                  <h1>Keys of success are too many, <br/>but one key of failure is "UNEDUCATED"</h1>
                 </div>
               </div>
             </div>
